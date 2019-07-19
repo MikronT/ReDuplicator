ff0b7a70d27afb59ad927aac7eec5987abeb9f16
MikronT <45333177+MikronT@users.noreply.github.com>
2019-07-19 17:13:16 +0300

    Meet the multithreading multisessioning search
    With bugs

--- reDuplicator.cmd
+++ reDuplicator.cmd
@@ -34,11 +34,14 @@ set argument=%1
 set argument=%argument:"=%
 
 if exist "%argument%" ( set directory=%argument%
-) else for /f "tokens=1,* delims=- " %%i in ("%*") do if "%%i" NEQ "" set key_%%i
+) else for /f "tokens=1,2,* delims=- " %%i in ("%*") do (
+  if "%%i" NEQ "" set key_%%i
+  if "%%j" NEQ "" set key_%%j
+)
 
 call :directoryChecker
 
-if "%key_call%" == "scan" call :scan
+if "%key_call%" NEQ "" call :%key_call% %key_thread%
 
 if exist temp rd /s /q temp
 
@@ -95,7 +98,7 @@ echo.
 
 
 if "%command%" == "1" (
-  start "" "%~dpnx0" --call=scan
+  start "" "%~dpnx0" --call=scan_preparing
   call :screen_scan
 )
 if "%command%" == "2" call :screen_settings
@@ -112,23 +115,22 @@ goto :screen_main
 :screen_scan
 %logo%
 
+set counter_duplicates=0
+for /l %%i in (1, 1, %setting_multithreading_threads%) do if exist %temp%\counter_duplicates%%i for /f "delims=" %%j in (%temp%\counter_duplicates%%i) do set /a counter_duplicates+=%%j
+
+echo.
 if exist %temp%\messages type %temp%\messages
 echo.
-if exist %temp%\counter_filesScanned for /f "delims=" %%i in (%temp%\counter_filesScanned) do echo.^(i^) Files scanned:   %%i
-if exist %temp%\counter_duplicates   for /f "delims=" %%i in (%temp%\counter_duplicates) do   echo.^(i^) Duplicate pairs: %%i
+if exist %temp%\counter_filesScanned1 for /f "delims=" %%i in (%temp%\counter_filesScanned1) do echo.^(i^) Files scanned:   %%i
+echo.^(i^) Duplicate pairs: %counter_duplicates%
 echo.
 
 if exist "%temp%\session_completed" (
-  if exist %log_duplicates% (
-    echo.^(i^) All info saved into the %log_duplicates% file>>%temp%\messages
-    for /f "delims=" %%i in ("%log_duplicates%") do echo.^(i^) Log file size: %%~zi bytes>>%temp%\messages
-  ) else echo.^(^!^) Any duplicates not found>>%temp%\messages
   echo.
   echo.
   echo.
   echo.^(i^) Press Enter to go to the main menu
   pause>nul
-  echo.>%temp%\session_closed
   exit /b
 )
 
@@ -143,10 +145,10 @@ goto :screen_scan
 
 
 
-:scan
-%logo%
-echo.>%temp%\duplicates
+:scan_preparing
+%logo% Scan Controller
 
+echo.>%temp%\duplicates
 set counter_log=0
 
 :cycle_log_name
@@ -157,36 +159,83 @@ if exist "%log_duplicates%" goto :cycle_log_name
 
 
 echo.^(i^) Getting directory tree...>>%temp%\messages
+set counter_dataLines=0
 
-for /f "delims=" %%i in ('dir /a:-d /b /s "%directory%\*%setting_filter_include%*"') do for /f "delims=" %%j in ("%%i") do echo.%%i;%%~zj>>%temp%\data
+for /f "delims=" %%i in ('dir /a:-d /b /s "%directory%\*%setting_filter_include%*"') do for /f "delims=" %%j in ("%%i") do (
+  set /a counter_dataLines+=1
+  echo.%%i;%%~zj>>%temp%\data
+)
 
 for /f "delims=" %%i in ("%temp%\data") do echo.^(i^) Directory tree data size: %%~zi bytes>>%temp%\messages
 echo.>>%temp%\messages
 
 
 
-echo.^(i^) Initialization...
+echo.^(i^) Initialization...>>%temp%\messages
 timeout /nobreak /t 3 >nul
 
+set /a multithreading_linesPerThread=%counter_dataLines%/%setting_multithreading_threads%+1
+set counter_thread=1
+set counter_dataLines_min=0
+set counter_dataLines=0
+set counter_dataLines_max=%multithreading_linesPerThread%
+call :cycle_multithread_initializing
 
 
-echo.^(i^) Starting files comparing...>>%temp%\messages
 
+echo.^(i^) Starting file comparing threads...>>%temp%\messages
+for /l %%i in (1, 1, %setting_multithreading_threads%) do start "" "%~dpnx0" --call=scan --thread=%%i
+
+
+
+:cycle_scanWait
+timeout /nobreak /t 1 >nul
+for /f "delims=" %%i in ('tasklist /fi "IMAGENAME eq cmd.exe" /fi "WINDOWTITLE eq %app_name% Thread*" ^| find /i /c "cmd.exe"') do if "%%i" == "0" goto :cycle_scanWait_outPoint
+goto :cycle_scanWait
+:cycle_scanWait_outPoint
+
+
+
+timeout /nobreak /t 1 >nul
+echo.>%temp%\session_completed
+
+
+
+echo.^(i^) Completed^!>>%temp%\messages
+echo.>>%temp%\messages
+
+if exist %log_duplicates% (
+  echo.^(i^) All info saved into the %log_duplicates% file>>%temp%\messages
+  for /f "delims=" %%i in ("%log_duplicates%") do echo.^(i^) Log file size: %%~zi bytes>>%temp%\messages
+) else echo.^(^!^) Any duplicates not found>>%temp%\messages
+exit
+
+
+
+
+
+
+
+
+
+:scan
+%logo%
+title %app_name% Thread %1
 setlocal EnableDelayedExpansion
 set counter_filesScanned=0
 set counter_duplicates=0
 
 for /f "tokens=1,2,* delims=;" %%i in ('type %temp%\data ^| find /i /v "%setting_filter_exclude%"') do (
   set /a counter_filesScanned+=1
-  echo.!counter_filesScanned!>%temp%\counter_filesScanned
-  for /f "tokens=1,2,* delims=;" %%o in ('type %temp%\data ^| find /i /v "%setting_filter_exclude%"') do (
+  echo.!counter_filesScanned!>%temp%\counter_filesScanned%1
+  for /f "tokens=1,2,* delims=;" %%o in ('type %temp%\data_thread%1 ^| find /i /v "%setting_filter_exclude%"') do (
     if "%%i" NEQ "%%o" if "%%j" == "%%p" (
       for /f "skip=1 tokens=2* delims=:" %%k in ('%module_rehash% -sha1 "%%i"') do (
         for /f "skip=1 tokens=2* delims=:" %%q in ('%module_rehash% -sha1 "%%o"') do (
           if "%%k" == "%%q" (
             for /f "delims=" %%z in ('type %temp%\duplicates ^| find /i /c "%%i"') do if "%%z" == "0" (
               set /a counter_duplicates+=1
-              echo.!counter_duplicates!>%temp%\counter_duplicates
+              echo.!counter_duplicates!>%temp%\counter_duplicates%1
 
               echo.%%i>>%temp%\duplicates
               echo.%%o>>%temp%\duplicates
@@ -231,21 +280,9 @@ for /f "tokens=1,2,* delims=;" %%i in ('type %temp%\data ^| find /i /v "%setting
     )
   )
 )
+
 endlocal
-
-timeout /nobreak /t 3 >nul
-echo.>%temp%\session_completed
-
-
-
-echo.^(i^) Scan completed^!
-echo.^(i^) Completed^!>>%temp%\messages
-
-
-
-:cycle_closeWait
-if exist "%temp%\session_closed" exit
-goto :cycle_closeWait
+exit
 
 
 
@@ -318,7 +355,8 @@ goto :screen_settings
 
 
 :logo
-title [MikronT] %app_name% %app_version% ^| %directory%
+if "%*" == "" ( title [MikronT] %app_name% %app_version% ^| %directory%
+) else title [MikronT] %app_name% %app_version% ^| %directory% ^| %*
 color 0b
 cls
 echo.
@@ -371,4 +409,40 @@ if "%setting_debug%" == "true" (
   echo.>>%log_duplicates%
 )
 echo.>>%log_duplicates%
+exit /b
+
+
+
+
+
+
+
+
+
+:cycle_multithread_initializing
+call :multithread_data_writing
+
+if "%counter_thread%" == "%setting_multithreading_threads%" exit /b
+
+set /a counter_thread+=1
+set /a counter_dataLines_min+=%multithreading_linesPerThread%
+set /a counter_dataLines_max+=%multithreading_linesPerThread%
+goto :cycle_multithread_initializing
+
+
+
+
+
+
+
+
+
+:multithread_data_writing
+setlocal EnableDelayedExpansion
+for /f "delims=" %%i in (%temp%\data) do (
+  set /a counter_dataLines+=1
+  if !counter_dataLines! GTR %counter_dataLines_min% echo.%%i>>%temp%\data_thread%counter_thread%
+  if "!counter_dataLines!" == "%counter_dataLines_max%" exit /b
+)
+endlocal
 exit /b
